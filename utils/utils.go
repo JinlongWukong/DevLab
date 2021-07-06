fa5594693f5bc548d981359419a27393b883e196
jinlliu <jinlliu@cisco.com>
2021-07-06 14:43:33 +0800

    random string bugfix

--- utils/utils.go
+++ utils/utils.go
@@ -11,6 +11,7 @@ import (
 	"sort"
 	"strconv"
 	"strings"
+	"time"
 )
 
 func HttpSendJsonData(uri string, method string, data []byte) (error, []byte) {
@@ -118,6 +119,7 @@ func RandomString(n int) string {
 	var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
 
 	s := make([]rune, n)
+	rand.Seed(time.Now().UnixNano())
 	for i := range s {
 		s[i] = letters[rand.Intn(len(letters))]
 	}
